d888a18f66933223c1c99eb0bee2232af698c6d9
benniho03 <ben.holderle.21@lehre.mosbach.dhbw.de>
2022-03-22 22:37:31 +0100

    Refactoring und Camera Follow

--- PopUp_Game/Assets/Scripts/PlayerMovement.cs
+++ PopUp_Game/Assets/Scripts/PlayerMovement.cs
@@ -6,20 +6,27 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float movementSpeed = 1;
-    public Collider2D otherCollider;
-    ForceMode2D m_ForceMode;
-    Vector2 m_NewForce;
+    Vector2 bounce;
+    Vector2 movement;
     Rigidbody2D rb;
     void Start(){
         rb = GetComponent<Rigidbody2D>();
     }
 
     void Update(){
-        var movement = Input.GetAxis("Horizontal");
-        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * movementSpeed;
+        movement = new Vector2(Input.GetAxis("Horizontal"), 0);
+    }
+    private void FixedUpdate() {
+        moveCharacter(movement);
     }
     private void OnCollisionEnter2D(Collision2D other) {
-        m_NewForce = new Vector2(0, 12.0f);
-        rb.AddForce(m_NewForce, ForceMode2D.Impulse);
+        bounceOnCollision();
+    }
+    void bounceOnCollision(){
+        bounce = new Vector2(0, 12.0f);
+        rb.AddForce(bounce, ForceMode2D.Impulse);
+    }
+    void moveCharacter(Vector2 direction){
+        rb.AddForce(direction / 3, ForceMode2D.Impulse);
     }
 }
