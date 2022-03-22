175d201412a3008a45824a101d7e1e2da2156bde
benniho03 <ben.holderle.21@lehre.mosbach.dhbw.de>
2022-03-22 16:36:51 +0100

    Added side option for platform

--- PopUp_Game/Assets/Scripts/Platform.cs
+++ PopUp_Game/Assets/Scripts/Platform.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
+
     // Start is called before the first frame update
+    public bool isLeft;
     public Collider2D otherCollider;
     Rigidbody2D rb;
     Vector2 m_NewForce;
@@ -20,7 +22,12 @@ public class Platform : MonoBehaviour
     }
 
     private void OnCollisionEnter2D(Collision2D other) {
-        m_NewForce = new Vector2(2f, 0);
+        if(isLeft){
+            m_NewForce = new Vector2(2f, 0);
+        } else {
+            m_NewForce = new Vector2(-2f, 0);
+        }
         rb.AddForce(m_NewForce, ForceMode2D.Impulse);
+
     }
 }
