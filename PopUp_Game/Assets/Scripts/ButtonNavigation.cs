3e1b6d890758210963969c93933241e22c958b78
on21FU <flo.uhlschmiedmx.de>
2022-05-12 12:34:21 +0200

    add functions to buttons

--- PopUp_Game/Assets/Scripts/ButtonNavigation.cs
+++ PopUp_Game/Assets/Scripts/ButtonNavigation.cs
@@ -13,11 +13,18 @@ public class ButtonNavigation : MonoBehaviour
     public void changeToGameScreen()
     {
         SceneManager.LoadScene(sceneName:"Main");
+        Time.timeScale = 1;
     }
     public void changeToSelectCharacterScreen(){
         SceneManager.LoadScene(sceneName:"selectCharacterScreen");
     }
     public void changeCharacterNr(int nr){
         CharacterSpriteHandler.characterNr = nr;
+    }
+    public void pauseGame(){
+        Time.timeScale = 0;
+    }
+    public void playGame(){
+        Time.timeScale = 1;
     }
 }
