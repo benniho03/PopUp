d46252f09cc065a598a6e6935675ca24af26449c
paulhnn26 <pau.henn.21@lehre.mosbach.dhbw.de>
2022-05-19 16:29:33 +0200

    Soundeffect

---
+++ PopUp_Game/Assets/Scripts/Soundeffect.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soundeffect : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other) {
+       GetComponent<AudioSource>().Play();
+   }
+}
